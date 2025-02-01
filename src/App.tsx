159e296b74f73e4fe8a16af8205e3a73d47cb17a
vbnair <vbnair988@gmail.com>
2025-02-01 02:25:14 +0100

    Initial frontend setup for dev

---
+++ src/App.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import reactLogo from "../public/assets/react.svg";
+import viteLogo from "../public/vite.svg";
+import "./styles/App.css";
+// import { small } from "framer-motion/client";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <div>
+        <a href="https://vite.dev" target="_blank">
+          <img src={viteLogo} className="logo" alt="Vite logo" />
+        </a>
+        <a href="https://react.dev" target="_blank">
+          <img src={reactLogo} className="logo react" alt="React logo" />
+        </a>
+      </div>
+      <h1>Vite + React</h1>
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "center",
+          alignItems: "center",
+          gap: "10px",
+        }}
+      >
+        <h3>Letscapes ...</h3>
+        <p style={{ fontSize: "small" }}>Version 1.0</p>
+      </div>
+
+      <div className="card">
+        <button onClick={() => setCount((count) => count + 1)}>
+          count is {count}
+        </button>
+        <p>
+          Edit <code>src/App.tsx</code> and save to test HMR
+        </p>
+      </div>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  );
+}
+
+export default App;
